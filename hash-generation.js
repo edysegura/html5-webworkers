32b00052323da8d2e0e1feb657b36244e7fdafd4
Edy Segura <ednardo@inatel.br>
2018-01-01 23:00:02 -0200

    Take the business logic to it's on file

---
+++ hash-generation.js
@@ -0,0 +1,11 @@
+function generateHash() {
+    return "id" + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17)
+}
+
+function generateHashes() {
+    const hashes = []
+    for (let i = 0; i < 10000; i++) {
+        hashes.push(generateHash())
+    }
+    return hashes
+}
--- index.js
+++ index.js
@@ -1,15 +1,4 @@
 
-function generateHash() {
-    return "id" + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17) + Math.random() * Math.pow(10, 17)
-}
-
-function generateHashes() {
-    const hashes = []
-    for (let i = 0; i < 10000; i++) {
-        hashes.push(generateHash())
-    }
-    return hashes
-}
 
 const button = document.querySelector('button')
 

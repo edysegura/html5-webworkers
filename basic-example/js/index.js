82043db261ba0e6ec44f3fafa5b23034faa420e8
Edy Segura <edysegura@gmail.com>
2018-08-20 17:59:46 -0300

    Implemented user choice

--- basic-example/js/index.js
+++ basic-example/js/index.js
@@ -1,9 +1,30 @@
-const worker = new Worker("js/worker.js");
+function delegateLaborTask() {
+  let isWorkerSelected = document.querySelector('[name=useWebWorker]').checked
+  isWorkerSelected
+    ? useWebWorker()
+    : useMainThread();
+}
 
-worker.addEventListener("message", (e) => {
-  // Receive results from worker thread
-  console.log("received: " + e.data.toString());
-}, false);
+function useWebWorker() {
+  const worker = new Worker('js/worker.js')
 
-// Send child process some work
-worker.postMessage(1000000000);
+  worker.addEventListener('message', event => {
+    // Receive results from worker thread
+    showResults(event.data)
+  })
+
+  // Send child process some work
+  worker.postMessage(1000000000)
+}
+
+function useMainThread() {
+  showResults('To be implemented!')
+}
+
+function showResults(result) {
+  const output = document.getElementById('output')
+  output.textContent = `received: ${result}`
+}
+
+const button = document.querySelector('button')
+button.addEventListener('click', delegateLaborTask)

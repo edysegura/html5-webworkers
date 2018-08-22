f3d0b6c23308a9142a6a3c60a7b0eba9ff547a67
Edy Segura <edysegura@gmail.com>
2018-08-22 15:44:50 -0300

    Import was commentted until browsers have support to it

--- es-module/js/worker.js
+++ es-module/js/worker.js
@@ -1,4 +1,12 @@
-import { heavyOperation } from './labor-task.js'
+// import { heavyOperation } from './labor-task.js'
+
+function heavyOperation(operationTimes) {
+  let result = 0
+  while (operationTimes--) {
+    result += Math.random()
+  }
+  return result
+}
 
 self.addEventListener('message', event => {
   let result = heavyOperation(event.data)

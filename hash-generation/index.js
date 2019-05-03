10de514f40c7f69c03f3ba577638cf36ecfb11c9
Edy Segura <edysegura@gmail.com>
2019-05-02 22:48:09 -0300

    Refactor hash generator example

--- hash-generation/index.js
+++ hash-generation/index.js
@@ -1,38 +1,37 @@
 'use strict'
 
-function delegateHashGeneration() {
-  let useWorker = document.querySelector('[name=useWebWorker]').checked
-  if (useWorker) {
-    generateWithWebWorker()
-  } else {
-    generateWithoutWebWorker()
-  }
+function delegateHashGenerator() {
+  const useWorker = document.querySelector('[name=useWebWorker]').checked
+  useWorker
+    ? useWebWorker()
+    : useMainThread()
 }
 
-function generateWithWebWorker() {
-  const method = 'with webwork'
+function useWebWorker() {
+  const method = 'webwork'
+  console.time(method)
   const worker = new Worker('worker.js')
-  console.time(method)
-  worker.addEventListener('message', e => {
-    // console.log(e.data)
-    handleHashes(e.data, method)
+  worker.addEventListener('message', event => {
+    const hashes = event.data
+    hashesHandler(hashes, method)
   })
-  worker.postMessage(true)
+  worker.postMessage('generate hashes')
 }
 
-function generateWithoutWebWorker() {
-  const method = 'without webwork'
+function useMainThread() {
+  const method = 'main thread'
   console.time(method)
-  const hashes = generateHashes().join('\n')
-  handleHashes(hashes, method)
+  const hashes = generateHashes()
+  hashesHandler(hashes, method)
 }
 
-function handleHashes(hashes, method) {
+function hashesHandler(hashes, method) {
   console.timeEnd(method)
-  console.log(`Done ${method}!`)
+  const numberOfHashes = hashes.length
+  console.log(`Done using ${method}! ${numberOfHashes} hashes were generated`)
 }
 
 const button = document.querySelector('button')
 button.addEventListener('click', () => {
-  delegateHashGeneration()
+  delegateHashGenerator()
 })

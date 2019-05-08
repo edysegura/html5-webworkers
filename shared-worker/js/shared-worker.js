b4f183e67020b3cfb75a16304415c14765f81210
Edy Segura <edysegura@gmail.com>
2019-05-07 22:18:18 -0300

    Formatting shared-worker.js

--- shared-worker/js/shared-worker.js
+++ shared-worker/js/shared-worker.js
@@ -5,7 +5,7 @@ const messagesLog = []
 self.onconnect = event => {
   const [ port ] = event.ports
   port.onmessage = event => {
-    messagesLog.push(`${ Date.now() } - Message from ${event.data}`)
+    messagesLog.push(`${Date.now()} - Message from ${event.data}`)
     port.postMessage(messagesLog)
   }
 }

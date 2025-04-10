8f4d10c999197463db063fe95480e7e36c26d1b8
Edy Segura <edysegura@gmail.com>
2025-04-10 17:27:27 -0300

    feat: migrate logic from index.js to app.js and update progress bar functionality

--- using-vite/src/js/app.js
+++ using-vite/src/js/app.js
@@ -1,35 +1,30 @@
 import { isWorkerSelected, showResults } from './html.service.js'
 import { heavyOperation } from './labor-task.js'
 
+const progressBar = document.getElementById('processProgress')
 let progressInterval
 
 function updateProgressBar() {
-  const progressBar = document.getElementById('processProgress')
   let progress = 0
-
   // Clear any existing interval
   if (progressInterval) {
     clearInterval(progressInterval)
     progressBar.value = 0
   }
-
   progressInterval = setInterval(() => {
-    progress += 1
+    progress += 5
     progressBar.value = progress
-
     if (progress >= 100) {
       clearInterval(progressInterval)
       progressBar.value = 0
     }
-  }, 250)
+  }, 500)
 }
 
 function delegateLaborTask() {
   let operationTimes = 1000000000
-
   showResults()
   updateProgressBar()
-
   isWorkerSelected()
     ? useWebWorker(operationTimes)
     : useMainThread(operationTimes)
@@ -37,15 +32,13 @@ function delegateLaborTask() {
 
 function useWebWorker(operationTimes) {
   const worker = new Worker('src/js/worker.js', { type: 'module' })
-
   worker.addEventListener('message', (event) => {
     showResults(event.data)
     clearInterval(progressInterval)
-    document.getElementById('processProgress').value = 100
+    progressBar.value = 100
     worker.terminate()
     console.log('[worker] Worker terminated')
   })
-
   worker.postMessage(operationTimes)
 }
 
@@ -53,7 +46,7 @@ function useMainThread(operationTimes) {
   let result = heavyOperation(operationTimes)
   showResults(result)
   clearInterval(progressInterval)
-  document.getElementById('processProgress').value = 100
+  progressBar.value = 100
   console.log('[main] Heavy operation completed')
 }
 

bb745a444edfdd8fa1078bedb1edb67852d9619d
Edy Segura <edysegura@gmail.com>
2025-04-30 17:31:57 -0300

    feat: add textarea for displaying worker messages and implement auto-scroll functionality

--- shared-worker/js/app.js
+++ shared-worker/js/app.js
@@ -2,8 +2,11 @@
 
 const url = new URL(location.href)
 const sharedWorker = new SharedWorker('js/shared-worker.js')
+const logElement = document.getElementById('log')
 sharedWorker.port.postMessage(url.pathname)
 
 sharedWorker.port.onmessage = (event) => {
   console.log(event.data)
+  logElement.value += event.data.join('\n')
+  logElement.scrollTop = logElement.scrollHeight // Auto-scroll to bottom
 }

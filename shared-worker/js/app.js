d80d69ceaf5109ddfe87c0c363ce65f23271f618
Edy Segura <edysegura@gmail.com>
2025-05-01 17:12:40 -0300

    refactor: update app.js and shared-worker.js to improve message logging and structure

--- shared-worker/js/app.js
+++ shared-worker/js/app.js
@@ -1,9 +1,11 @@
-'use strict'
-
-const url = new URL(location.href)
 const sharedWorker = new SharedWorker('js/shared-worker.js')
+const url = new URL(location.href)
 const logElement = document.getElementById('log')
-sharedWorker.port.postMessage(url.pathname)
+
+sharedWorker.port.postMessage({
+  url: url.pathname,
+  visibility: document.visibilityState,
+})
 
 sharedWorker.port.onmessage = (event) => {
   console.log(event.data)
--- shared-worker/js/shared-worker.js
+++ shared-worker/js/shared-worker.js
@@ -1,11 +1,11 @@
-'use strict'
-
 const messagesLog = []
 
 self.onconnect = (event) => {
   const [port] = event.ports
   port.onmessage = (event) => {
-    messagesLog.push(`${Date.now()} - Message from ${event.data}`)
+    messagesLog.push(
+      `${Date.now()} - Message from ${JSON.stringify(event.data)}`
+    )
     port.postMessage(messagesLog)
   }
 }
